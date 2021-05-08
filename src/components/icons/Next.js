30d7005bb7a91ec22d918c13a3e821ea08a36599
mehmetalicayhan <mehmetalicyhn@gmail.com>
2021-05-09 01:33:32 +0300

    chore: icons and markers

---
+++ src/components/icons/Next.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function SvgNext(props) {
+  return (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 36 36"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M18 0C8.074 0 0 8.074 0 18c0 9.925 8.074 18 18 18 9.925 0 18-8.075 18-18 0-9.926-8.075-18-18-18zm5.56 19.06l-7.5 7.5a1.496 1.496 0 01-2.12 0 1.498 1.498 0 010-2.12L20.379 18l-6.44-6.44a1.498 1.498 0 010-2.12 1.498 1.498 0 012.122 0l7.5 7.5a1.498 1.498 0 010 2.12z"
+        fill="#132C33"
+      />
+    </svg>
+  );
+}
+
+export default SvgNext;
---
+++ src/components/map/markers/index.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {Marker} from 'react-map-gl';
+import {colorArray} from "../utilities/Colors";
+
+const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
+  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
+  C20.1,15.8,20.2,15.8,20.2,15.7z`;
+
+
+function Markers(props) {
+    const {data, onClick} = props;
+
+
+    return data.map((coordinate, index) => {
+        return coordinate.locations.map((loc, i) => {
+            return <Marker className="z-10" key={`marker-${i}`} longitude={loc[0].longitude}
+                           latitude={loc[0].latitude}>
+                <svg
+                    height={22}
+                    viewBox="0 0 24 24"
+                    style={{
+                        cursor: 'pointer',
+                        stroke: 'none',
+                        transform: `translate(${-22 / 2}px,${-22}px)`
+                    }}
+                    onClick={() => onClick(loc[0])}
+                >
+                    <path fill={colorArray[index]} d={ICON}/>
+                    <text fill="black" style={{zIndex: 100}} x="50%" y="50%" dominantBaseline="middle"
+                          textAnchor="middle">{i + 1}</text>
+
+                </svg>
+            </Marker>
+
+        })
+
+    })
+
+}
+
+export default Markers;
