69026b06167102e6de056910519e419152c2ae18
mehmetalicayhan <mehmetalicyhn@gmail.com>
2021-05-19 01:55:40 +0300

    feat: followers following icons

---
+++ src/components/layout/users.js
@@ -0,0 +1,11 @@
+export const users = [
+    {id: 1, name: "ali", surname: "çayhan",username:"mcayhan"},
+    {id: 1, name: "ali", surname: "çayhan",username:"mcayhan"},
+    {id: 1, name: "ali", surname: "çayhan",username:"mcayhan"},
+    {id: 1, name: "ali", surname: "çayhan",username:"mcayhan"},
+    {id: 1, name: "ali", surname: "çayhan",username:"mcayhan"},
+    {id: 1, name: "ali", surname: "çayhan",username:"mcayhan"},
+    {id: 1, name: "ali", surname: "çayhan",username:"mcayhan"},
+    {id: 1, name: "ali", surname: "çayhan",username:"mcayhan"}
+]
+
